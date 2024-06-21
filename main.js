c2cdd9a4eff99b1cd97d986f29801a4cfb2c2963
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2024-06-21 13:41:38 -0400

    Add toolbar & remove from taskbar on minimize

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require('electron')
 const { Steam } = require('./steam/steam')
 const { dracFlowLyrics } = require('./dracula-flow')
 const path = require('node:path')
 
 let steamObj = new Steam();
+
+let win, tray;
 
 ipcMain.on('submit-login', (_event, arg) => {
     steamObj.logOn(arg.accountName, arg.password);
@@ -18,9 +20,10 @@ ipcMain.on('stop-nickname', (_event, _arg) => {
 })
 
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    skipTaskbar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -46,6 +49,13 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
+  tray = new Tray(path.join(__dirname, 'draculaflow.ico'))
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show', click: () => win.show() },
+    { label: 'Quit', click: () => win.close() },
+  ])
+  tray.setToolTip('This shit aint nothin to me man')
+  tray.setContextMenu(contextMenu)
   createWindow()
 
   app.on('activate', () => {
